81399832ae2a9328a15ef18fb9ba339bc3b30f1e
Shyam Venkatesh <shyamvenkat007@gmail.com>
2023-02-27 23:37:10 +0530

    Last working commit as of 23:37

---
+++ scripts/toggle-collapsible.js
@@ -0,0 +1,7 @@
+// console.log("here");
+// let collapsedTags = document.getElementsByClassName("collapsible");
+// console.log("here");
+
+// for(let i = 0; i < collapsedTags.length; i++) {
+//     collapsedTags. = 
+// }
