fb4e9a1ffdfadb19e95eebe8026e4b3ea8d93e71
Shyam Venkatesh <shyamvenkat007@gmail.com>
2023-03-01 08:51:42 +0530

    Last working  state as of 08:51

---
+++ scripts/text-slideshow.js
@@ -0,0 +1,25 @@
+let slideIndex = 1;
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[n-1].style.display = "block";
+    dots[n-1].className += " active";
+} 
+
+showSlides(1);
+
+let handler = setInterval(() => {
+    if(++slideIndex > 5) {
+        slideIndex = 1;
+    }
+    showSlides(slideIndex);
+}, 5000);
