617cf79725ac8eee102e725e4de52b5cae2dd51f
Shyam Venkatesh <shyamvenkat007@gmail.com>
2023-03-20 21:01:31 +0530

    Last working state as of 21:00

---
+++ scripts/animate.js
@@ -0,0 +1,27 @@
+let contentElem = document.getElementsByClassName('content-section-invisible');
+let sectionElem = document.getElementsByClassName("animation-section")[0];
+let imageHolderElem = document.getElementsByClassName("changing-image")[0];
+
+let options = { 
+    rootMargin: "-300px 0px -300px 0px",
+};
+
+const iObserver = new IntersectionObserver((items) => {
+    for(let item of items) {
+        if(item.isIntersecting) {
+            item.target.classList.add('content-section-visible');
+
+            let imageId = item.target.className.match(/\d/);
+            console.log("here", imageId);
+
+            imageHolderElem.src = `/images/scrolling-mockup/neopop-fold${imageId}.webp`
+        } else {
+            item.target.classList.remove('content-section-visible');
+        }
+    }
+}, 
+options);
+
+for (i = 0; i < contentElem.length; i++) {
+  iObserver.observe(contentElem[i]);
+}
